0cdf1f2c79f89d5a42f36c41888690b1085c6660
masakanobuhiro <engineernobu.study@gmail.com>
2020-06-02 18:30:11 +0900

    first commit

---
+++ Assets/ShotBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBall : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float power = 1f;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    rigidbody = GetComponent<Rigidbody>();
+                    rigidbody.AddForce(ray.direction * power, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
